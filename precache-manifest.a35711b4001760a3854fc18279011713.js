4292e8e61391c78797ce107977502d6950974b64
dembit <dembit1991@mail.ru>
2020-05-01 10:28:01 +0300

    Updates

---
+++ precache-manifest.a35711b4001760a3854fc18279011713.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2a5f5664df07068fa2fc889c79c270",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "b076783b6c935f72d519",
+    "url": "/counter/static/js/2.4d57918f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.4d57918f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d8f1675b34867d5886",
+    "url": "/counter/static/js/main.d954a2a9.chunk.js"
+  },
+  {
+    "revision": "371931ef5b3745d18686",
+    "url": "/counter/static/js/runtime-main.0c7a0bf3.js"
+  }
+]);
